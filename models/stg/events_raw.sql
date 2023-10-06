bb6bec275cc3e88d6c1355643813f41ce813a5c1
sorin.silvestru.v@gmail.com <sorin.silvestru.v@gmail.com>
2023-10-06 19:38:00 +0000

    configured stg model

--- models/stg/events_raw.sql
+++ models/stg/events_raw.sql
@@ -1,11 +1,32 @@
-{% set config = get_stg_config() %}
-
-
-select 1 as test
-from `{{ config["project"] }}.{{ config["dataset"] }}.events_intraday_*`
-{% if target.name == 'default' %} --TODO: what's the target name of deployed jobs?
-where _TABLE_SUFFIX >=  --create macro + adapter
-{% endif %}
-
---target_name: {{target.name}}
---{{config}}
+select
+    *
+    date(timestamp_micros(fb.event_timestamp)) as event_date,
+    date_diff(
+        date(timestamp_micros(fb.event_timestamp)),
+        date(timestamp_micros(user_first_touch_timestamp)),
+        day
+    ) as age,
+    user_pseudo_id,
+    user_id,
+    event_name
+    user_first_touch_timestamp as installed_date,
+    struct<revenue float64, currency string>(
+        user_ltv.revenue, user_ltv.currency
+    ) as users_ltv,
+    struct<
+        type string,
+        brand_name string,
+        model_name string,
+        marketing_name string,
+        os_hardware_model string
+    >(
+        device.category,
+        device.mobile_brand_name,
+        device.mobile_model_name,
+        device.mobile_marketing_name,
+        device.mobile_os_hardware_model
+    ) as device
+    struct<operating_system string, operating_system_version string>(
+        device.operating_system, device.operating_system_version
+    ) as device_os
+from {{ source("firebase_events", "events") }}
--- models/stg/mandatory_vals.sql
+++ models/stg/mandatory_vals.sql
@@ -1,4 +1,6 @@
 
+{{ config(materialized='table', sort='timestamp', dist='user_id') }}
+
 {% if var("OVERBASE:FIREBASE_DATASET_ID") == 'overbase' and env_var('DBT_INSIDE_OVERBASE','')|length == 0 %}
    {{ exceptions.raise_compiler_error("Invalid variable. Got: overbase_project" ~ number) }}
 {% endif %}
