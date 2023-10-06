2c2124f5afd46638f77edbfdf28a38620f50a3bf
sorin.silvestru.v@gmail.com <sorin.silvestru.v@gmail.com>
2023-10-06 09:08:29 +0000

    mandatory variables

--- models/stg/events_raw.sql
+++ models/stg/events_raw.sql
@@ -3,5 +3,9 @@
 
 select 1 as test
 from `{{ config["project"] }}.{{ config["dataset"] }}.events_intraday_*`
+{% if target.name == 'default' %} --TODO: what's the target name of deployed jobs?
+where _TABLE_SUFFIX >=  --create macro + adapter
+{% endif %}
 
-
+--target_name: {{target.name}}
+--{{config}}
---
+++ models/stg/mandatory_vals.sql
@@ -0,0 +1,4 @@
+
+{% if var("OVERBASE:FIREBASE_DATASET_ID") == 'overbase' and env_var('DBT_INSIDE_OVERBASE')|length == 0 %}
+   {{ exceptions.raise_compiler_error("Invalid variable. Got: overbase_project" ~ number) }}
+{% endif %}
