0d079712d0511c8da77ee2e88f937e245072f203
sorin.silvestru.v@gmail.com <sorin.silvestru.v@gmail.com>
2023-10-06 09:19:46 +0000

    default var

--- models/stg/mandatory_vals.sql
+++ models/stg/mandatory_vals.sql
@@ -1,4 +1,4 @@
 
-{% if var("OVERBASE:FIREBASE_DATASET_ID") == 'overbase' and env_var('DBT_INSIDE_OVERBASE')|length == 0 %}
+{% if var("OVERBASE:FIREBASE_DATASET_ID") == 'overbase' and env_var('DBT_INSIDE_OVERBASE','')|length == 0 %}
    {{ exceptions.raise_compiler_error("Invalid variable. Got: overbase_project" ~ number) }}
 {% endif %}
