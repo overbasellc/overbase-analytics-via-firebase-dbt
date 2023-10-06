fcf59066d12247fbb2262f14f5ef3180984ec397
sorin.silvestru.v@gmail.com <sorin.silvestru.v@gmail.com>
2023-10-06 19:48:20 +0000

    fix vars

--- models/stg/mandatory_vals.sql
+++ models/stg/mandatory_vals.sql
@@ -1,6 +1,6 @@
 
-{{ config(materialized='table', sort='timestamp', dist='user_id') }}
+{{ config(materialized='ephemeral') }}
 
-{% if var("OVERBASE:FIREBASE_DATASET_ID") == 'overbase' and env_var('DBT_INSIDE_OVERBASE','')|length == 0 %}
+{% if var("OVERBASE:FIREBASE_DATASET_ID",'overbase') == 'overbase' and env_var('DBT_INSIDE_OVERBASE','')|length == 0 %}
    {{ exceptions.raise_compiler_error("Invalid variable. Got: overbase_project" ~ number) }}
 {% endif %}
