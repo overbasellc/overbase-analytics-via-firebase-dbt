68d8e202bb1031c4d48776e4c301d0c7c08a3186
Adi Racu <adrian.racu@gmail.com>
2023-10-08 20:04:25 +0000

    added `compile_mandatory_var` to mandate that integrating overwrite certain variables in their dbt_project.yml

--- models/stg/events_raw.sql
+++ models/stg/events_raw.sql
@@ -1,24 +1,24 @@
-select
-    *
-    date(timestamp_micros(fb.event_timestamp)) as event_date,
-    date_diff(
-        date(timestamp_micros(fb.event_timestamp)),
-        date(timestamp_micros(user_first_touch_timestamp)),
-        day
-    ) as age,
-    user_pseudo_id,
-    user_id,
-    event_name
-    user_first_touch_timestamp as installed_date,
-    struct<revenue float64, currency string>(
+SELECT
+    -- *
+      DATE(timestamp_micros(event_timestamp)) as event_date
+    , DATE_DIFF(
+        DATE(timestamp_micros(event_timestamp)),
+        DATE(timestamp_micros(user_first_touch_timestamp)),
+        DAY
+    ) as age
+    , user_pseudo_id
+    , user_id
+    , event_name
+    , user_first_touch_timestamp as installed_date
+    , STRUCT<revenue FLOAT64, currency STRING>(
         user_ltv.revenue, user_ltv.currency
-    ) as users_ltv,
-    struct<
-        type string,
-        brand_name string,
-        model_name string,
-        marketing_name string,
-        os_hardware_model string
+    ) as users_ltv
+    , STRUCT<
+        type STRING,
+        brand_name STRING,
+        model_name STRING,
+        marketing_name STRING,
+        os_hardware_model STRING
     >(
         device.category,
         device.mobile_brand_name,
@@ -26,7 +26,7 @@ select
         device.mobile_marketing_name,
         device.mobile_os_hardware_model
     ) as device
-    struct<operating_system string, operating_system_version string>(
+    , STRUCT<operating_system STRING, operating_system_version STRING>(
         device.operating_system, device.operating_system_version
     ) as device_os
-from {{ source("firebase_events", "events") }}
+from {{ source("firebase_analytics", "events") }}
---
+++ models/stg/mandatory_vars.sql
@@ -0,0 +1,11 @@
+
+{{ config(materialized='ephemeral') }}
+
+
+{{ compile_mandatory_var("OVERBASE:FIREBASE_PROJECT_ID", "overbase") }}
+{{ compile_mandatory_var("OVERBASE:FIREBASE_ANALYTICS_DATASET_ID", "firebase_analytics_raw_test") }}
+{{ compile_mandatory_var("OVERBASE:FIREBASE_ANALYTICS_EVENTS_TABLE_NAME", "events_*") }}
+{{ compile_mandatory_var("OVERBASE:FIREBASE_ANALYTICS_EVENTS_INTRADAY_TABLE_NAME", "events_intraday_*") }}
+ 
+
+ SELECT 1
