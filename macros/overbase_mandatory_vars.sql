815577b7e8a0945969f1e8440a5a1e383ebe4c33
Adi Racu <adrian.racu@gmail.com>
2023-10-08 21:52:50 +0000

    trying to make the macro work in clients

--- macros/overbase_mandatory_vars.sql
+++ macros/overbase_mandatory_vars.sql
@@ -1,19 +1,17 @@
 -- a variable that must be overwritten when OB Analytics via FB is used in another DBT project
 -- the default value is meant only to be used within this project, as test values 
-{% macro compile_time_mandatory_var(variable_name, default_value_in_ob) %}
-
-{% if var(variable_name, default_value_in_ob) == default_value_in_ob and env_var('DBT_INSIDE_OVERBASE','')|length == 0 %}
+{% macro compile_time_mandatory_var(variable_name, default_value_in_ob) -%}
+{% if var(variable_name, default_value_in_ob) == default_value_in_ob and env_var('DBT_INSIDE_OVERBASE','')|length == 0 -%}
    {{ exceptions.raise_compiler_error("Variable '%s' must be overwritten inside your dbt_project.yml. Check the Overbase documentation to see all mandatory variables" % variable_name) }}
-{% endif %}
+{% else %}
+{%- endif %}
+{%- endmacro %}
 
-
-{% endmacro %}
-
-{% macro verify_all_overbase_mandatory_variables() %}
-
-
+{% macro verify_all_overbase_mandatory_variables() -%}
 {{ compile_time_mandatory_var("OVERBASE:FIREBASE_PROJECT_ID", "overbase") }}
 {{ compile_time_mandatory_var("OVERBASE:FIREBASE_ANALYTICS_DATASET_ID", "firebase_analytics_raw_test") }}
- 
-
-{% endmacro %}
+{{ compile_time_mandatory_var("OVERBASE:FIREBASE_PROJECT_ID", "overbase") }}
+{{ compile_time_mandatory_var("OVERBASE:FIREBASE_PROJECT_ID", "overbase") }}
+{{ compile_time_mandatory_var("OVERBASE:FIREBASE_PROJECT_ID", "overbase") }}
+{{ compile_time_mandatory_var("OVERBASE:FIREBASE_PROJECT_ID", "overbase") }}
+{%- endmacro %}
--- models/stg/events_raw.sql
+++ models/stg/events_raw.sql
@@ -3,32 +3,16 @@
 SELECT
     -- *
       DATE(timestamp_micros(event_timestamp)) as event_date
-    , DATE_DIFF(
-        DATE(timestamp_micros(event_timestamp)),
-        DATE(timestamp_micros(user_first_touch_timestamp)),
-        DAY
-    ) as age
+    , DATE_DIFF(DATE(timestamp_micros(event_timestamp)),DATE(timestamp_micros(user_first_touch_timestamp)),DAY) as age
     , user_pseudo_id
     , user_id
     , event_name
     , user_first_touch_timestamp as installed_date
     , STRUCT<revenue FLOAT64, currency STRING>(
-        user_ltv.revenue, user_ltv.currency
-    ) as users_ltv
-    , STRUCT<
-        type STRING,
-        brand_name STRING,
-        model_name STRING,
-        marketing_name STRING,
-        os_hardware_model STRING
-    >(
-        device.category,
-        device.mobile_brand_name,
-        device.mobile_model_name,
-        device.mobile_marketing_name,
-        device.mobile_os_hardware_model
-    ) as device
+        user_ltv.revenue, user_ltv.currency) as users_ltv
+    , STRUCT<type STRING,brand_name STRING,model_name STRING,marketing_name STRING,os_hardware_model STRING>(
+        device.category,device.mobile_brand_name,device.mobile_model_name,device.mobile_marketing_name,device.mobile_os_hardware_model ) as device
     , STRUCT<operating_system STRING, operating_system_version STRING>(
-        device.operating_system, device.operating_system_version
-    ) as device_os
-from {{ source("firebase_analytics", "events") }}  
+        device.operating_system, device.operating_system_version) as device_os
+FROM {{ source("firebase_analytics", "events") }}  
+
