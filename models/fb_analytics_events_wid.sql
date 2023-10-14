f71f296f6ceb6a4b7a204dd2ae8ccba17e57079d
Adrian Racu <adrian.racu@gmail.com>
2023-10-15 01:57:57 +0300

    user variables that exist

--- models/fb_analytics_events_wid.sql
+++ models/fb_analytics_events_wid.sql
@@ -27,15 +27,15 @@
 
 {%- set miniColumnsToIgnoreInGroupBy = eventParamsToIgnoreInGroupBy + userPropertiesToIgnoreInGroupBy -%}
 
-{%- set tmp_res = overbase_firebase.get_filtered_columns_for_table("fb_analytics_events_raw", columnNamesEventDimensions, miniColumnsToIgnore) -%}
+{%- set tmp_res = overbase_firebase.get_filtered_columns_for_table("fb_analytics_events_raw", columnNamesEventDimensions, miniColumnsToIgnoreInGroupBy) -%}
 {%- set columnsForEventDimensions = tmp_res[0] -%}
 {%- set eventDimensionsUnnestedCount = tmp_res[1]  -%}
 
-{%- set tmp_res = overbase_firebase.get_filtered_columns_for_table("fb_analytics_installs_raw", columnNamesInstallDimensions, miniColumnsToIgnore) -%}
+{%- set tmp_res = overbase_firebase.get_filtered_columns_for_table("fb_analytics_installs_raw", columnNamesInstallDimensions, miniColumnsToIgnoreInGroupBy) -%}
 {%- set columnsForInstallDimensions = tmp_res[0] -%}
 {%- set installDimensionsUnnestedCount = tmp_res[1]  -%}
 {# do these separaetely, so we don't end up with install_installed_dates #}
-{%- set tmp_res = overbase_firebase.get_filtered_columns_for_table("fb_analytics_installs_raw", ["installed_dates"], miniColumnsToIgnore) -%}
+{%- set tmp_res = overbase_firebase.get_filtered_columns_for_table("fb_analytics_installs_raw", ["installed_dates"], miniColumnsToIgnoreInGroupBy) -%}
 {%- set columnsForInstalledDatesDimension = tmp_res[0] -%}
 {%- set installedDatesDimensionsUnnestedCount = tmp_res[1]  -%}
 
@@ -48,11 +48,11 @@
 
 WITH data as (
     SELECT    DATE(events.created_at) as created_date
-            , {{ overbase_firebase.unpack_columns_into_minicolumns(columnsForEventDimensions, miniColumnsToIgnore, "events.", "event_") }}
+            , {{ overbase_firebase.unpack_columns_into_minicolumns(columnsForEventDimensions, miniColumnsToIgnoreInGroupBy, "events.", "event_") }}
 
             , DATE(installs.installed_at) as installed_date
-            , {{ overbase_firebase.unpack_columns_into_minicolumns(columnsForInstalledDatesDimension, miniColumnsToIgnore, "installs.", "") }}
-            , {{ overbase_firebase.unpack_columns_into_minicolumns(columnsForInstallDimensions, miniColumnsToIgnore, "installs.", "install_") }}
+            , {{ overbase_firebase.unpack_columns_into_minicolumns(columnsForInstalledDatesDimension, miniColumnsToIgnoreInGroupBy, "installs.", "") }}
+            , {{ overbase_firebase.unpack_columns_into_minicolumns(columnsForInstallDimensions, miniColumnsToIgnoreInGroupBy, "installs.", "install_") }}
             , COUNT(1) as cnt
             , COUNT(DISTINCT(events.user_pseudo_id)) as users
             {{ ", " if custom_summed_metrics|length > 0 else "" }} {{ custom_summed_metrics |map(attribute='agg')|join(", ") }}
@@ -63,10 +63,10 @@ WITH data as (
     GROUP BY 1,2 {% for n in range(3, 3 + eventDimensionsUnnestedCount + installedDatesDimensionsUnnestedCount + installDimensionsUnnestedCount) -%} ,{{ n }} {%- endfor %}
 )
 SELECT created_date
-        , {{ overbase_firebase.pack_minicolumns_into_structs_for_select(columnsForEventDimensions, miniColumnsToIgnore, "event_", "") }}
+        , {{ overbase_firebase.pack_minicolumns_into_structs_for_select(columnsForEventDimensions, miniColumnsToIgnoreInGroupBy, "event_", "") }}
         , installed_date
-        , {{ overbase_firebase.pack_minicolumns_into_structs_for_select(columnsForInstalledDatesDimension, miniColumnsToIgnore, "", "") }}
-        , {{ overbase_firebase.pack_minicolumns_into_structs_for_select(columnsForInstallDimensions, miniColumnsToIgnore, "install_", "install_") }}
+        , {{ overbase_firebase.pack_minicolumns_into_structs_for_select(columnsForInstalledDatesDimension, miniColumnsToIgnoreInGroupBy, "", "") }}
+        , {{ overbase_firebase.pack_minicolumns_into_structs_for_select(columnsForInstallDimensions, miniColumnsToIgnoreInGroupBy, "install_", "install_") }}
         , cnt
         , users
         , {{ custom_summed_metrics |map(attribute='alias')|join(", ") }}
