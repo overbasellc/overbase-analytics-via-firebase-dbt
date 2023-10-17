cfcba5801840159422e81785509cd1ea16703c54
Adi @ MBP M1 <adrian.racu@gmail.com>
2023-10-17 11:35:53 +0300

    added profiles.yml to make it work with local dbt

--- models/test.sql
+++ models/test.sql
@@ -5,5 +5,3 @@
 {{ get_event_parameter_tuples_for_rollup_dimensions() | map(attribute=0) | list }}
 
 
-
-{{ get_event_parameter_tuples_for_rollup_dimensions_to_ignore() | map(attribute=0) | list }}
