5f329857cc018c5c974f4040d32a708f6e642312
Adi @ MBP M1 <adrian.racu@gmail.com>
2023-10-18 00:40:58 +0300

    minimal ui tables

--- models/fb_analytics_ui.sql
+++ models/fb_analytics_ui.sql
@@ -1,11 +1,12 @@
 {{ config(
     materialized='table',
     partition_by={
-      "field": "created_at",
-      "data_type": "timestamp",
+      "field": "created_date",
+      "data_type": "date",
       "granularity": "day"
      }
 ) }}
 
 SELECT *
 FROM {{ ref("fb_analytics_events") }}
+WHERE event_name IN ('ob_ui_view_shown', 'ob_ui_button_tapped', 'ui_view_shown', 'ui_button_tapped')
