a90f0882ba803bb01e7cc76a39dc907238ee1b14
Adi @ MBP M1 <adrian.racu@gmail.com>
2023-10-25 10:44:38 +0300

    also update the `user_id` in install_raw with the oldest user_id found

--- models/fb_analytics_installs_raw.sql
+++ models/fb_analytics_installs_raw.sql
@@ -28,22 +28,36 @@ WITH  custom_install_event AS (
         SELECT * FROM {{ ref('fb_analytics_events_raw') }} WHERE event_name = 'first_open' AND {{ overbase_firebase.analyticsTSFilterFor('event_ts') }}
         QUALIFY ROW_NUMBER() OVER (PARTITION BY user_pseudo_id ORDER BY event_ts) = 1
 )
+, user_pseudo_id_to_user_id AS (
+        SELECT user_pseudo_id, user_id
+        FROM {{ ref('fb_analytics_events_raw') }} WHERE user_id IS NOT NULL AND {{ overbase_firebase.analyticsTSFilterFor('event_ts') }}
+        QUALIFY ROW_NUMBER() OVER (PARTITION BY user_pseudo_id ORDER BY event_ts) = 1
+)
 
 {%- set miniColumnsToIgnoreInGroupBy = ["duplicates_cnt"] -%}
 {%- set columns = overbase_firebase.get_filtered_columns_for_table("fb_analytics_events_raw", "*", miniColumnsToIgnoreInGroupBy)[0] -%}
 
+{%- set columnsInSelect = [] -%}
+{%- for column in columns %}
+    {%- if column.name == 'user_id' -%}
+        {%- set _ = columnsInSelect.append("users.user_id") -%}
+    {%- else -%}
+        {%- set _ = columnsInSelect.append("COALESCE(custom." ~ column.name ~ ", ob." ~ column.name ~ ", fb." ~ column.name ~ ") as " ~ column.name) -%}
+    {%- endif -%}
+{%- endfor %}
+
+
 , data as (
-    SELECT   
-        {%- for column in columns %}
-            {{ ", " if not loop.first else "" }} COALESCE(custom.{{ column.name }}, ob.{{ column.name }}, fb.{{ column.name }}) as {{ column.name }}
-        {%- endfor %}
+    SELECT   {{ columnsInSelect | join("\n           , ") }} 
     FROM fb_install_event as fb
     FULL OUTER JOIN ob_install_event as ob ON fb.user_pseudo_id = ob.user_pseudo_id
     FULL OUTER JOIN custom_install_event as custom ON fb.user_pseudo_id = custom.user_pseudo_id
+    FULL OUTER JOIN user_pseudo_id_to_user_id as users ON COALESCE(fb.user_pseudo_id, ob.user_pseudo_id, custom.user_pseudo_id) = users.user_pseudo_id
     WHERE True 
 )
 -- SELECT  COUNT(1) , COUNT(DISTINCT(user_pseudo_id))
 SELECT *
 FROM data 
+
 
 
