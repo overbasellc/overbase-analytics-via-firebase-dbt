a5223c95de5cbd82ca602dbdbdf7b22932ef1f34
Adi @ MBP M1 <adrian.racu@gmail.com>
2023-10-25 12:56:00 +0300

    crashlytics date filters

--- macros/dateFilters.sql
+++ macros/dateFilters.sql
@@ -3,11 +3,20 @@
 	{{ dateField }} BETWEEN DATE({{ startEndTSTuple[0] }}) AND DATE({{ startEndTSTuple[1] }})
 {%- endmacro -%}
 
+{%- macro crashlyticsDateFilterFor(dateField) -%}
+	{%- set startEndTSTuple = overbase_firebase.crashlyticsStartEndTimestampsTuple() -%}
+	{{ dateField }} BETWEEN DATE({{ startEndTSTuple[0] }}) AND DATE({{ startEndTSTuple[1] }})
+{%- endmacro -%}
+
 {%- macro analyticsTSFilterFor(tsField) -%}
 	{%- set startEndTSTuple = overbase_firebase.analyticsStartEndTimestampsTuple() -%}
 	{{ tsField }} BETWEEN {{ startEndTSTuple[0] }} AND {{ startEndTSTuple[1] }}	
 {%- endmacro -%}
 
+{%- macro crashlyticsTSFilterFor(tsField) -%}
+	{%- set startEndTSTuple = overbase_firebase.crashlyticsStartEndTimestampsTuple() -%}
+	{{ tsField }} BETWEEN {{ startEndTSTuple[0] }} AND {{ startEndTSTuple[1] }}	
+{%- endmacro -%}
 
 {%- macro analyticsTableSuffixFilter() -%}
 	{%- set startEndTSTuple = overbase_firebase.analyticsStartEndTimestampsTuple() -%}
@@ -31,6 +40,23 @@
 			{%- set tsEnd = "TIMESTAMP(DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY))" -%}
 		{%- else -%}
 			{%- set tsEnd = "TIMESTAMP('" ~ var('OVERBASE:FIREBASE_ANALYTICS_FULL_REFRESH_END_DATE', "")  ~ "')" -%}
+		{%- endif -%}
+		{{ return((tsStart, tsEnd)) }}
+	{%- endif -%}
+{%- endmacro -%}
+
+
+{%- macro crashlyticsStartEndTimestampsTuple() -%}
+	{%- if is_incremental() -%}
+		{%- set tsStart = "TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL " ~ var('OVERBASE:FIREBASE_CRASHLYTICS_DEFAULT_INCREMENTAL_DAYS', "5")  ~ " DAY)" -%}
+		{%- set tsEnd = "TIMESTAMP(DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY))" -%}
+		{{ return((tsStart, tsEnd)) }}
+	{%- else -%}
+		{%- set tsStart = "TIMESTAMP('" ~ var('OVERBASE:FIREBASE_CRASHLYTICS_FULL_REFRESH_START_DATE', '2018-01-01') ~ "')" -%}
+		{%- if var('OVERBASE:FIREBASE_CRASHLYTICS_FULL_REFRESH_END_DATE', "nada") == 'nada' -%}
+			{%- set tsEnd = "TIMESTAMP(DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY))" -%}
+		{%- else -%}
+			{%- set tsEnd = "TIMESTAMP('" ~ var('OVERBASE:FIREBASE_CRASHLYTICS_FULL_REFRESH_END_DATE', "")  ~ "')" -%}
 		{%- endif -%}
 		{{ return((tsStart, tsEnd)) }}
 	{%- endif -%}
