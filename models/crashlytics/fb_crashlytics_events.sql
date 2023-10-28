d88ae9f3b2804e532e0cacab8b300082643979de
Adi @ MBP M1 <adrian.racu@gmail.com>
2023-10-29 01:05:48 +0300

    add a rollup model for fb crashlytics

---
+++ models/crashlytics/fb_crashlytics_events.sql
@@ -0,0 +1,38 @@
+{{ config(
+    materialized='incremental',
+    partition_by={
+      "field": "event_date",
+      "data_type": "date",
+      "granularity": "day"
+     },
+    incremental_strategy = 'insert_overwrite',
+    require_partition_filter = true
+) }}
+
+
+{%- set columnNamesEventDimensions = ["app_id", "issue", "platform", "error_type", "process_state"
+                                     ,"orientation", "app_version", "platform_version", "jailbroken_state"
+                                     ,"device_hardware", "custom_keys"
+] -%}
+
+{%- set miniColumnsToIgnoreInGroupBy = [] -%}
+{%- set tmp_res = overbase_firebase.get_filtered_columns_for_table("fb_crashlytics_events_raw", columnNamesEventDimensions, miniColumnsToIgnoreInGroupBy) -%}
+{%- set columnsForEventDimensions = tmp_res[0] -%}
+{%- set eventDimensionsUnnestedCount = tmp_res[1]  -%}
+
+WITH data as (
+    SELECT   DATE(event_ts) as event_date
+            , {{ overbase_firebase.unpack_columns_into_minicolumns(columnsForEventDimensions, miniColumnsToIgnoreInGroupBy, [], "", "") }}
+            , COUNT(1) as cnt -- same as COUNT(DISTINCT(event_id))
+            , COUNT(DISTINCT(crashlytics_user_pseudo_id)) as users
+    FROM {{ ref("fb_crashlytics_events_raw") }}
+    WHERE {{ overbase_firebase.analyticsTSFilterFor('event_ts') }}
+    GROUP BY 1,2 {% for n in range(3, 3 + eventDimensionsUnnestedCount) -%} ,{{ n }} {%- endfor %}
+)
+SELECT event_date
+        , install_age_group
+        , {{ overbase_firebase.pack_minicolumns_into_structs_for_select(columnsForEventDimensions, miniColumnsToIgnoreInGroupBy, "", "") }}
+        , cnt
+        , users
+        {{ ", " if custom_summed_metrics|length > 0 else "" }}  {{ custom_summed_metrics |map(attribute='alias')|join(", ") }}
+FROM data
