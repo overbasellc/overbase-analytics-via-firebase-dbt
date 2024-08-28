61dfe2615e719393e390db78f939228d91b72d32
SorinSV <121880489+SorinSV@users.noreply.github.com>
2024-08-28 22:49:29 +0300

    fix test

--- tests/events_raw_incremental.sql
+++ tests/events_raw_incremental.sql
@@ -3,16 +3,18 @@
 ) }}
 
 WITH stg AS (
-SELECT event_date, COUNT(*) AS cnt FROM {{ ref('fb_analytics_events_raw') }} 
-WHERE {{ overbase_firebase.analyticsDateFilterFor('event_date',extend=2) }}
+SELECT event_date, SUM(duplicates_cnt) AS cnt FROM {{ ref('fb_analytics_events_raw') }} 
+WHERE {{ overbase_firebase.analyticsTestDateFilter('event_date',extend=2) }}
+and event_date <= current_date -5
 GROUP BY 1
 )
 , src AS (
 
-SELECT event_date,COUNT(*) AS cnt
+SELECT DATE(TIMESTAMP_MICROS(event_timestamp)) as event_date,COUNT(*) AS cnt
 FROM {{ source("firebase_analytics", "events") }}
-WHERE {{ overbase_firebase.analyticsTableSuffixFilter(extend =3) }}
-AND {{ overbase_firebase.analyticsDateFilterFor('event_date',extend=2) }}
+WHERE {{ overbase_firebase.analyticsTestTableSuffixFilter(extend = 3) }}
+AND {{ overbase_firebase.analyticsTestDateFilter('DATE(TIMESTAMP_MICROS(event_timestamp))',extend=2) }}
+AND DATE(TIMESTAMP_MICROS(event_timestamp)) <= current_date -5 --buffer because firebase keeps refreshing the recent partitions
 GROUP BY 1
 )
 select * from 
