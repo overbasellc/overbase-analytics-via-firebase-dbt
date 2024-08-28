e18c4e0df29dc158e50dbd3076ab936d8e6a3437
SorinSV <121880489+SorinSV@users.noreply.github.com>
2024-08-28 22:46:04 +0300

    add macros for tests date filters

--- macros/dateFilters.sql
+++ macros/dateFilters.sql
@@ -1,3 +1,12 @@
+{%- macro analyticsTestDateFilter(fieldName, extend = 0) -%}
+    {{ ccDateFilterFor(fieldName, forceIncremental = True, extend = extend) }} 
+{%- endmacro %}
+
+{%- macro analyticsTestTableSuffixFilter(extend =0) -%}
+	{%- set startEndTSTuple = overbase_firebase.analyticsStartEndTimestampsTuple(forceIncremental = True, extend=extend + 1) -%}  {# extended by one day because TABLE_SUFFIX is not always UTC #}
+  REPLACE(_TABLE_SUFFIX, 'intraday_', '') BETWEEN FORMAT_DATE('%Y%m%d', {{ startEndTSTuple[0] }}) AND FORMAT_DATE('%Y%m%d', {{ startEndTSTuple[1] }})
+{%- endmacro -%}
+
 {%- macro analyticsDateFilterFor(dateField,extend = 0) -%}
 	{%- set startEndTSTuple = overbase_firebase.analyticsStartEndTimestampsTuple(extend = extend) -%}
 	{{ dateField }} BETWEEN DATE({{ startEndTSTuple[0] }}) AND DATE({{ startEndTSTuple[1] }})
@@ -23,8 +32,8 @@
   REPLACE(_TABLE_SUFFIX, 'intraday_', '') BETWEEN FORMAT_DATE('%Y%m%d', {{ startEndTSTuple[0] }}) AND FORMAT_DATE('%Y%m%d', {{ startEndTSTuple[1] }})
 {%- endmacro -%}
 
-{%- macro analyticsStartEndTimestampsTuple(extend = 0) -%}
-	{%- if is_incremental() -%}
+{%- macro analyticsStartEndTimestampsTuple(forceIncremental = False, extend = 0) -%}
+	{%- if (forceIncremental or is_incremental()) -%}
         {%- set INCREMENTAL_DAYS =  var('OVERBASE:FIREBASE_ANALYTICS_DEFAULT_INCREMENTAL_DAYS', 5 ) +extend  -%}
 		{%- set tsStart = "TIMESTAMP_SUB(TIMESTAMP(CURRENT_DATE()), INTERVAL " ~  INCREMENTAL_DAYS  ~ " DAY)" -%}
 		{%- set tsEnd = "TIMESTAMP(DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY))" -%}
