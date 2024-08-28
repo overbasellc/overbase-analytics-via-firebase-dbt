485a4a07e9a9d781e60119a37d386bd7a0cdf780
SorinSV <121880489+SorinSV@users.noreply.github.com>
2024-08-28 22:49:02 +0300

    fix test

--- tests/analytics_installs_unique.sql
+++ tests/analytics_installs_unique.sql
@@ -1,7 +1,8 @@
 WITH counts AS (
 SELECT COUNT(DISTINCT user_pseudo_id) AS dist_cnt,count(1) AS cnt
-FROM {{ref('fb_analytics_events_raw')}}
-WHERE {{ overbase_firebase.analyticsDateFilterFor('event_date',extend = 2) }}
+FROM {{ref('fb_analytics_installs_raw')}}
+WHERE {{ overbase_firebase.analyticsTestDateFilter('event_date',extend = 2) }}
+AND event_date >='2024-08-22' --date when incrementel model was fixed
 )
 
 SELECT * FROM counts WHERE dist_cnt < cnt
