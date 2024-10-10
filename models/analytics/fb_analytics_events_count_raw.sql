c61b7ebfa506d1bf7c6116a452dc9d4479c80b0a
SorinSV <121880489+SorinSV@users.noreply.github.com>
2024-10-10 15:36:29 +0300

    Create fb_analytics_events_count_raw.sql

---
+++ models/analytics/fb_analytics_events_count_raw.sql
@@ -0,0 +1,22 @@
+{{ config(
+    materialized='incremental',
+    partition_by={
+      "field": "event_date",
+      "data_type": "date",
+      "granularity": "day"
+     },
+    incremental_strategy = 'insert_overwrite',
+    require_partition_filter = true,
+) }}
+
+
+
+SELECT
+    event_date
+    ,user_id
+    ,user_pseudo_id
+    ,count(*) as events_count
+FROM  {{ ref("fb_analytics_events_raw") }}
+WHERE {{ overbase_firebase.analyticsDateFilterFor('event_date') }}
+GROUP by 1,2,3
+
